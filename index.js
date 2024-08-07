cbeed9b1e560b74509ba1822c499901f6ce423a8
Verbiturum <143966105+verbey@users.noreply.github.com>
2024-08-07 03:56:58 +0300

    Add newRouter

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
-const indexRouter = require('./routes/indexRouter');
 const path = require('node:path');
+
+const indexRouter = require('./routes/indexRouter');
+const newRouter = require('./routes/newRouter');
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
+
+app.use('/new', newRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
